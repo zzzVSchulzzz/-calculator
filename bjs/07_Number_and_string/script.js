d4c0b23c192237f8874a2ff4d5aced15a06c5d59
avsudnichnikov <avsudnichnikov@gmail.com>
2020-08-07 00:41:06 +0500

    init & add work #7

---
+++ bjs/07_Number_and_string/script.js
@@ -0,0 +1,12 @@
+let lastOperand = 0;
+let operation = null;
+
+const inputWindow = document.getElementById('inputWindow');
+
+
+document.getElementById('btn_clr').addEventListener('click', function () {
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = '';
+})
+
