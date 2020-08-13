14013ee904391e5727c2e3a14497573d5717772a
avsudnichnikov <avsudnichnikov@gmail.com>
2020-08-14 01:24:38 +0500

    add work #8

---
+++ bjs/08_if_else/script.js
@@ -0,0 +1,49 @@
+let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+let answer = Math.floor((minValue + maxValue) / 2);
+let orderNumber = 1;
+let gameRun = true;
+
+const orderNumberField = document.getElementById('orderNumberField');
+const answerField = document.getElementById('answerField');
+
+orderNumberField.innerText = orderNumber;
+answerField.innerText = `Вы загадали число ${answer}?`;
+
+document.getElementById('btnRetry').addEventListener('click', function () {
+    minValue = 0;
+    maxValue = 100;
+    orderNumber = 0;
+})
+
+document.getElementById('btnOver').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            console.log(phraseRandom);
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            minValue = answer + 1;
+            answer = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answer}?`;
+        }
+    }
+})
+
+document.getElementById('btnEqual').addEventListener('click', function () {
+    if (gameRun){
+        minValue = answer;
+        maxValue = answer;
+        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        gameRun = false;
+    }
+})
+
