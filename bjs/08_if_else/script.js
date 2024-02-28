281890eede42aae61ba31b1b582dbc6474acc2f5
Valerija Schulz <135040463+zzzVSchulzzz@users.noreply.github.com>
2024-02-28 15:07:26 +0300

    Update script.js

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -244,3 +244,64 @@ function startGame() {
 
 //Запускаем игру
 startGame()
+
+
+
+/**
+ * // Функция для вывода числа текстом
+function numberInText(answerNumber) {
+
+    if (answerNumber.length <= 20) {
+        return useNumberInText()
+    } else {
+        return answerNumber
+    }
+}
+
+function useNumberInText(answerNumber) {
+    // Проверяем, является ли переданный аргумент числом
+    if(typeof answerNumber === 'number' && Number.isInteger(answerNumber) >= -999 && answerNumber <= 999) {
+        
+        // Создаем массивы с числами 0-19, с десятками и сотнями
+        if (answerNumber) {
+            const simpleNums = ['ноль', 'один', 'два','три','четыре','пять', 'шесть','семь','восемь','девять','десять','одиннадцать','двенадцать','тринадцать', 'четырнадцать','пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать'];
+            const tens = ['', '', 'двадцать', 'тридцать','сорок', 'пятьдесят','шестьдесят', 'семьдесят','восемьдесят', 'девяносто'];
+            const hundreds = ['', 'сто','двести', 'триста','четыреста', 'пятьсот','шестьсот', 'семьсот','восемьсот', 'девятьсот'];
+
+            if (answerNumber === 0) {
+                return ' ноль'
+            } // обработка нуля
+
+            let result = ''
+
+            if (answerNumber < 0) {
+                result += 'минус '
+                answerNumber = Math.abs(answerNumber) 
+            } // обработка отрицательных чисел (мы добавим слово минус, а само число станет положительным по модулю)
+
+            if (answerNumber >= 100) {
+                result += hundreds[Math.floor(answerNumber / 100)] + ' ';
+                answerNumber %= 100;
+            } // разбор сотен
+
+            if (answerNumber >= 20) {
+                result += tens[Math.floor(answerNumber / 10)] + ' ';
+                answerNumber %= 10;
+            } // разбор десятков
+
+            if (answerNumber > 0) {
+                if (answerNumber < 20) {
+                    result += simpleNums[answerNumber] + ' ';
+                } else {
+                    result += simpleNums[Math.floor(answerNumber % 10)] + ' ';
+                }
+            } // обработка 1-19
+            
+            return result.trim(); // убираем лишние пробелы в конце строки
+        } else {
+            return answerNumber
+        }
+    }
+}
+
+ */
